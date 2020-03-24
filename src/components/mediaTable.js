c4659386fd807a126b16a5a244eb98acd2513df8
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-03-25 01:36:33 +0200

    week1 task1

--- src/components/mediaTable.js
+++ src/components/mediaTable.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MediaRow from './mediaRow';
 
-const MediaTable = ({ media }) => {
+const MediaTable = () => {
+  const [picArray, setPicArray] = useState([]);
+  const loadMedia = async () => {
+    const response = await fetch('test.json');
+    const json = await response.json();
+    console.log(json);
+    setPicArray(json);
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
   return (
     <table>
       <tbody>
-        {media.map((file, index) => (
+        {picArray.map((file, index) => (
           <MediaRow file={file} key={index} />
         ))}
       </tbody>
